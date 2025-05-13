ef71e89bf477da573cac2dfbb33578d8b7a437c8
Tan Thanh <thanhdt969@gmail.com>
2025-05-13 16:07:46 +0700

    Add project files.

---
+++ Demo/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+
+namespace Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly HttpClient client = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = string.Empty;
+        }
+
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+
+            try
+            {
+                // Gửi yêu cầu GET và nhận chuỗi phản hồi
+                string responseBody = await client.GetStringAsync(uri);
+
+                // Gán nội dung HTML vào textbox
+                txtContent.Text = responseBody.Trim();
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Message: {ex.Message}");
+            }
+        }
+    }
+}
